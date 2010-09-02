6ac29ab41a6962e356aa638e13d8025996387e11
jonjlee@gmail.com <jonjlee@gmail.com>
2010-09-02 17:16:35 -0400

    Accept POSTs to /ev

--- src/events_resource.erl
+++ src/events_resource.erl
@@ -1,8 +1,33 @@
 -module(events_resource).
--export([init/1, to_html/2]).
+-export([
+	init/1, 
+	allowed_methods/2,
+	post_is_create/2,
+	create_path/2,
+	content_types_accepted/2,
+	accept_event_body/2
+	]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
-init([]) -> {ok, undefined}.
+init([]) -> 
+	{ok, undefined}.
 
-to_html(ReqData, Context) -> {"<html><body>Hello, new world</body></html>", ReqData, Context}.
+allowed_methods(RD, Ctx) -> 
+	{['POST'], RD, Ctx}.
+
+post_is_create(RD, Ctx) -> 
+	{true, RD, Ctx}.
+
+create_path(RD, Ctx) -> 
+	{"ka34x", RD, Ctx}.
+
+content_types_accepted(RD, Ctx) -> 
+	{[
+		{"application/json", accept_event_body}
+	], 
+	RD, 
+	Ctx}.
+
+accept_event_body(RD, Ctx) ->
+    {true, RD, Ctx}.
